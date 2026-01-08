bc8fa5f769117d30bdf26cd60671d6ad2ff744e0
DTomris <dotomris@gmail.com>
2026-01-08 09:57:30 +0400

    fix: Static export mode for Hostinger compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // TypeScript hatalarını görmezden gel (Siteyi yayınlamak için)
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    // ESLint hatalarını görmezden gel
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    env: {
-        NEXTAUTH_URL: 'http://localhost:3000',
-        NEXTAUTH_SECRET: 'tqta-secret-key-2024',
-    },
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
